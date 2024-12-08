d6f422bacbb437c7313a6cd5b33d03134197157f
Robert Ayrapetyan <robert@yag.im>
2024-12-07 17:43:25 -0800

    add: new companies

--- scripts/patches/200_patch_companies.sql
+++ scripts/patches/200_patch_companies.sql
@@ -1,3 +1,6 @@
 \c yag;
 
 INSERT INTO games.companies(id, name) VALUES (1000000, 'Седьмой волк');
+INSERT INTO games.companies(id, name) VALUES (1000001, 'Yavsoft');
+INSERT INTO games.companies(id, name) VALUES (1000002, 'Sibart');
+INSERT INTO games.companies(id, name) VALUES (1000003, 'Vochozka Trading');
