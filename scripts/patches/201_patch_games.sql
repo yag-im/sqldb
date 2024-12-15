36b4227aed42049ac496b61f87490a63d1461b13
Robert Ayrapetyan <robert@yag.im>
2024-12-14 17:28:36 -0800

    upd: riana rouge short descr

--- scripts/patches/201_patch_games.sql
+++ scripts/patches/201_patch_games.sql
@@ -3,3 +3,4 @@
 -- IGDB doesn't support custom ESRB ratings on their end, so patching them here
 UPDATE games.games set esrb_rating = 11 WHERE igdb->>'slug' = 'styrlitz';
 UPDATE games.games set esrb_rating = 11 WHERE igdb->>'slug' = 'red-comrades-save-the-galaxy';
+UPDATE games.games set short_descr = 'A full-motion video, point-and-click action-adventure game rated (AO) Adults Only. Hint: use Ctrl+F4 to swap CDs during the game.' WHERE igdb ->> 'slug' = 'riana-rouge';
