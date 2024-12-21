074f66d4dabaa012fa151dbac5fa8902c4cb6365
Robert Ayrapetyan <robert@yag.im>
2024-12-21 12:47:46 -0800

    fix: DMCA strike

--- scripts/patches/201_patch_games.sql
+++ scripts/patches/201_patch_games.sql
@@ -1,6 +1,17 @@
 \c yag;
 
 -- IGDB doesn't support custom ESRB ratings on their end, so patching them here
-UPDATE games.games set esrb_rating = 11 WHERE igdb->>'slug' = 'styrlitz';
-UPDATE games.games set esrb_rating = 11 WHERE igdb->>'slug' = 'red-comrades-save-the-galaxy';
+UPDATE games.games set esrb_rating = 11 WHERE igdb->>'slug' IN (
+    'styrlitz',
+    'red-comrades-save-the-galaxy'
+);
+
+
+-- DMCA https://lumendatabase.org/notices/47379973?utm_medium=panel
+UPDATE games.releases set is_visible = false where name IN (
+    'Myst'
+);
+
+-- custom descriptions
 UPDATE games.games set short_descr = 'A full-motion video, point-and-click action-adventure game rated (AO) Adults Only. Hint: use Ctrl+F4 to swap CDs during the game.' WHERE igdb ->> 'slug' = 'riana-rouge';
+
