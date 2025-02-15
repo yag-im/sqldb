c8034d92bf7648b856a99a081de0ee89db7c4a55
Robert Ayrapetyan <robert@yag.im>
2025-02-15 12:03:02 -0800

    fix: james bond hint

--- scripts/patches/201_patch_games.sql
+++ scripts/patches/201_patch_games.sql
@@ -6,7 +6,6 @@ UPDATE games.games set esrb_rating = 11 WHERE igdb->>'slug' IN (
     'red-comrades-save-the-galaxy'
 );
 
-
 -- DMCA https://lumendatabase.org/notices/47379973?utm_medium=panel
 UPDATE games.releases set is_visible = false where name IN (
     'Myst'
@@ -14,4 +13,4 @@ UPDATE games.releases set is_visible = false where name IN (
 
 -- custom descriptions
 UPDATE games.games set short_descr = 'A full-motion video, point-and-click action-adventure game rated (AO) Adults Only. Hint: use Ctrl+F4 to swap CDs during the game.' WHERE igdb ->> 'slug' = 'riana-rouge';
-
+UPDATE games.games set short_descr = short_descr || ' HINT: Search for "Stealth Affair Color Protection" to bypass the protection screen.' WHERE igdb ->> 'slug' = 'james-bond-007-the-stealth-affair';
