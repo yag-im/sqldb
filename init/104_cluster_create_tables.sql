21722ae249482d92aedb50c13ad4aa71e9e35c15
Robert Ayrapetyan <robert@yag.im>
2026-06-22 19:32:05 -0700

    refact: cluster.nodes table

--- init/104_cluster_create_tables.sql
+++ init/104_cluster_create_tables.sql
@@ -4,27 +4,17 @@
 CREATE TYPE cluster.region AS ENUM ('us-west-1', 'us-east-1');
 CREATE TYPE cluster.node_type AS ENUM ('dedicated', 'public-cloud-instance');
 CREATE TYPE cluster.node_flavor AS ENUM ('rise-3', 'b3-7', 'b3-8', 't2-le-45', 'l4-90');
+CREATE TYPE cluster.service_type AS ENUM ('jukebox', 'appstor');
 
-CREATE TABLE cluster.appstor_nodes
+CREATE TABLE cluster.nodes
 (
     id               BIGSERIAL PRIMARY KEY NOT NULL,
     uuid             UUID UNIQUE NOT NULL,
-    private_ip       INET UNIQUE NOT NULL,
     region           cluster.region NOT NULL,
+    service_type     cluster.service_type NOT NULL,
+    node_ix          SMALLINT NOT NULL,
     node_type        cluster.node_type NOT NULL,
-    flavor           cluster.node_flavor NOT NULL,
-    created_ts       TIMESTAMP NOT NULL DEFAULT NOW(),
-    share_id         SMALLINT NOT NULL
-);
-
-CREATE TABLE cluster.jukebox_nodes
-(
-    id               BIGSERIAL PRIMARY KEY NOT NULL,
-    uuid             UUID UNIQUE NOT NULL,
-    private_ip       INET UNIQUE NOT NULL,
-    public_ip        INET UNIQUE NOT NULL,
-    region           cluster.region NOT NULL,
-    node_type        cluster.node_type NOT NULL,
-    flavor           cluster.node_flavor NOT NULL,
+    node_flavor      cluster.node_flavor NOT NULL,
     created_ts       TIMESTAMP NOT NULL DEFAULT NOW()
 );
+CREATE UNIQUE INDEX cluster_nodes_uidx ON cluster.nodes (region, service_type, node_ix);
--- releases/2026-03-31.sql
+++ releases/2026-03-31.sql
@@ -2,60 +2,13 @@
 -- local env
 ALTER TYPE cluster.node_flavor ADD VALUE 'custom-1';
 
-INSERT INTO cluster.jukebox_nodes(uuid, private_ip, public_ip, region, node_type, flavor, created_ts)
+INSERT INTO cluster.nodes(uuid, region, service_type, node_ix, node_type, node_flavor, created_ts)
 VALUES (
     'f973b9a3-2d9b-4168-bbff-a9fa1d1bc5dc',
-    '172.17.0.1',
-    '172.17.0.1',
     'us-west-1',
+    'jukebox',
+    0, -- '172.17.0.1'
     'dedicated',
     'custom-1',
     '2026-01-01 00:00:00');
 */
-
--- only dedicated jukebox nodes should be inserted manually;
--- vm jukebox nodes are inserted automatically by the orchestrator
-INSERT INTO cluster.jukebox_nodes(uuid, private_ip, public_ip, region, node_type, flavor, created_ts)
-VALUES (
-   'd77f5742-b554-4374-8262-29512d68b684',
-   '192.168.12.2',
-   '135.148.169.16',
-   'us-east-1',
-   'dedicated',
-   'rise-3',
-   '2026-01-01 00:00:00'
-);
-
-INSERT INTO cluster.jukebox_nodes(uuid, private_ip, public_ip, region, node_type, flavor, created_ts)
-VALUES (
-   'ed9bd877-1337-4158-bad7-f97aecfce9e6',
-   '192.168.13.2',
-   '51.81.208.83',
-   'us-west-1',
-   'dedicated',
-   'rise-3',
-   '2026-01-01 00:00:00'
-);
-
--- appstor nodes should be inserted manually;
-INSERT INTO cluster.appstor_nodes(uuid, private_ip, region, node_type, flavor, share_id, created_ts)
-VALUES (
-    'b5157279-85ee-4d11-9399-8ab23a1baee3',
-    '192.168.12.200',
-    'us-east-1',
-    'public-cloud-instance',
-    'b3-8',
-    0,
-    '2026-01-01 00:00:00'
-);
-
-INSERT INTO cluster.appstor_nodes(uuid, private_ip, region, node_type, flavor, share_id, created_ts)
-VALUES (
-   '2a5c4e38-b969-495a-9149-cd632af65039',
-   '192.168.13.200',
-   'us-west-1',
-   'public-cloud-instance',
-   'b3-8',
-   0,
-   '2026-01-01 00:00:00'
-);
